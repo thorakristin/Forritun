f9c5ed5a32f042e9c64f644bfd706f52c99d5982
Þóra Kristín Klemenzdóttir <tkk8@hi.is>
2019-09-05 15:01:06 +0000

    Algorith for max_int.py

---
+++ Tímaverkefni/max_int.py
@@ -0,0 +1,14 @@
+# User puts in a number.
+# If the number is positive, the user always  puts in another number.
+# When the user puts in a negative number, 
+# the highest postivie number he had already put in will print.
+
+num_int = int(input("Input a number: "))    # Do not change this line
+max_int = 0
+
+while num_int >= 0:
+    if max_int > num_int:
+        max_int=num_int
+    num_int = int(input("Input a number: "))
+
+print("The maximum is", max_int)    # Do not change this line
